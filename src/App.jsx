cecf47449cf8b31849324f222f3a34b5d3693d96
Mri-dul <mriduloffi@gmail.com>
2025-04-12 23:53:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
           <div className="message bot-message">
             <Chatboticon />
             <p className="message-text">
-              hey debu <br /> how can i help you today?
+              hey debu I can be your personal bot<br /> how can i help you today?
             </p>
           </div>
 
